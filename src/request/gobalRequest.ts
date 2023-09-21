083f7fa2e315933f5d9fcd1aedc6f69b7bf5d866
cdeo <guohailiang152@163.com>
2023-09-21 21:44:44 +0800

    fix: 生产环境配置

--- src/request/gobalRequest.ts
+++ src/request/gobalRequest.ts
@@ -8,6 +8,7 @@ import {message} from "antd";
 
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
+  prefix: process.env.NODE_ENV === 'production' ? 'http://154.8.195.55:8080' : undefined
 });
 
 
